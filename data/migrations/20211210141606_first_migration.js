463d072dc6f9acd784c78faab9cd14f4c4a50d1f
Jeong Pak <dannypak716@gmail.com>
2021-12-10 09:30:52 -0500

    built out migrations.  last night after a couple hours of trying to figure out the issue with codegrade test, i realized that the issue was between my application and the database.  for some reason, the database wasn't recognizing my db.  i don't know what fixed it today because my migrations file is built out exactly the same as last night.  the only thing i can think of is in sprint 1, gabe was helping me with an error that could only be fixed by recloning the repo into a higher level folder....and that's what worked again this time.

--- data/migrations/20211210141606_first_migration.js
+++ data/migrations/20211210141606_first_migration.js
@@ -1,8 +1,53 @@
-
 exports.up = function(knex) {
+    return knex.schema
+      .createTable('projects', table => {
+          table.increments('project_id')
+          table.string('project_name').notNullable()
+          table.string('project_description')
+          table.boolean('project_completed').defaultTo(0)
+      })
+      .createTable('resources', table => {
+          table.increments('resource_id')
+          table.string('resource_name', 128).notNullable().unique()
+          table.string('resource_description')
+      })
+      .createTable('tasks', table => {
+          table.increments('task_id')
+          table.string('task_description').notNullable()
+          table.string('task_notes')
+          table.boolean('task_completed').defaultTo(0)
+          table.integer('project_id')
+              .unsigned()
+              .notNullable()
+              .references('project_id')
+              .inTable('projects')
+              .onUpdate('CASCADE')
+              .onDelete('CASCADE')
+      })
+      .createTable('project_resources', table => {
+          table.increments('project_resources_id')
+          table.integer('project_id')
+              .unsigned()
+              .notNullable()
+              .references('project_id')
+              .inTable('projects')
+              .onUpdate('CASCADE')
+              .onDelete('CASCADE')
+          table.integer('resource_id')
+              .unsigned()
+              .notNullable()
+              .references('resource_id')
+              .inTable('resources')
+              .onUpdate('RESTRICT')
+              .onDelete('RESTRICT')
+      })
+  };
   
-};
-
-exports.down = function(knex) {
+  exports.down = function(knex) {
   
-};
+    return knex.schema    
+      .dropTableIfExists('project_resources')
+      .dropTableIfExists('tasks')
+      .dropTableIfExists('resources')
+      .dropTableIfExists('projects')
+  };
